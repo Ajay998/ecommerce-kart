c9019a6eaa933dad6de67f3fc5bb22fe88601be4
Ajay Krishna Devadasan <54180151+Ajay998@users.noreply.github.com>
2025-09-10 21:29:02 +0530

    Category Model And User Custom Model

---
+++ GreatKart/category/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create a Category model for ecommerce website with (category_name, slug, description, cat_image 
+# with verbose name as category and data __str__ is category _name
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50,unique=True)
+    slug = models.CharField(max_length=100,unique=True)
+    description = models.CharField(max_length=255)
+    cat_image = models.ImageField(upload_to="photos/categories",blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
+        self.category_name
+
