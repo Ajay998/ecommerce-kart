c13429a8781210e08e7f8d2e3a45811fee9e7080
Ajay Krishna Devadasan <54180151+Ajay998@users.noreply.github.com>
2025-09-10 21:57:03 +0530

    Category Add Media Files

--- GreatKart/category/models.py
+++ GreatKart/category/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Category(models.Model):
     category_name = models.CharField(max_length=50,unique=True)
     slug = models.CharField(max_length=100,unique=True)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True)
     cat_image = models.ImageField(upload_to="photos/categories",blank=True)
 
     class Meta:
@@ -14,5 +14,5 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
     
     def __str__(self):
-        self.category_name
+        return self.category_name
 
