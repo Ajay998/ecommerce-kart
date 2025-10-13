508acd5ed7c061af81624f7ad9c19d3d9a89470d
Ajay Krishna Devadasan <54180151+Ajay998@users.noreply.github.com>
2025-10-13 19:46:45 +0530

    Implement pagination for product listing in store view

--- VeloraKart/store/views.py
+++ VeloraKart/store/views.py
@@ -3,6 +3,7 @@ from .models import Product
 from category.models import Category
 from cart.models import CartItem
 from cart.views import _cart_id
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def store(request, category_slug=None):
@@ -11,14 +12,31 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 6)  # Show 6 products per page
+        page = request.GET.get('page')
+        try:
+            paged_products = paginator.page(page)
+        except PageNotAnInteger:
+            paged_products = paginator.page(1)
+        except EmptyPage:
+            paged_products = paginator.page(paginator.num_pages)
+
         product_count = products.count()
         # Logic to filter products by category can be added here
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 6)  # Show 6 products per page
+        page = request.GET.get('page')
+        try:
+            paged_products = paginator.page(page)
+        except PageNotAnInteger:
+            paged_products = paginator.page(1)
+        except EmptyPage:
+            paged_products = paginator.page(paginator.num_pages)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
