3d00f665b947b58a274247a94b77ed5211575a58
Ajay Krishna Devadasan <54180151+Ajay998@users.noreply.github.com>
2025-10-18 22:22:07 +0530

    Add checkout functionality with views, URL routing, and checkout template and fix advanced search

--- VeloraKart/cart/views.py
+++ VeloraKart/cart/views.py
@@ -11,6 +11,7 @@ def _cart_id(request):
 
 def add_to_cart(request,product_id):
     product = Product.objects.get(id=product_id)
+    
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
@@ -68,7 +69,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
     except Cart.DoesNotExist:
         pass # just ignore
 
-    context = {
+    context = {             
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
@@ -77,4 +78,27 @@ def cart(request, total=0, quantity=0, cart_items=None):
     }
     return render(request, 'cart/cart.html', context)
     
+def checkout(request, total=0, quantity=0, cart_items=None):
+    total = 0
+    tax = 0
+    grand_total = 0
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
+    except Cart.DoesNotExist:
+        pass # just ignore
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total
+    }
+    return render(request, 'store/checkout.html', context)
 
