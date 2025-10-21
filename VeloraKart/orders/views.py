43463848b43a276cd4996a42832d6bca580bcf92
Ajay Krishna Devadasan <54180151+Ajay998@users.noreply.github.com>
2025-10-21 22:19:37 +0530

    Add order processing functionality with models, forms, views, and templates
    - Registered Payment model in admin
    - Created OrderForm for order data input
    - Added zip_code field to Order model
    - Implemented place_order view to handle order placement logic
    - Updated checkout template to include zip code input and form action
    - Configured URL routing for orders

--- VeloraKart/orders/views.py
+++ VeloraKart/orders/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
-
+import datetime
+from django.shortcuts import render, redirect
+from cart.models import CartItem
+from .forms import OrderForm
+from .models import Order
 # Create your views here.
+
+def place_order(request):
+    current_user = request.user
+    # Logic for placing an order will go here
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        # Redirect to store if cart is empty
+        return redirect('store')
+    
+    grand_total = 0
+    tax = 0
+    total = 0
+    quantity = 0
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    tax = (2 * total)/100
+    grand_total = total + tax
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            # Store all the billing information inside Order table
+            data = Order()
+            data.user = current_user
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone = form.cleaned_data['phone']
+            data.email = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.zip_code = form.cleaned_data['zip_code']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total = grand_total  # This should be calculated based on cart items
+            data.tax = tax  # This should be calculated based on cart items
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr,mt,dt)
+            current_date = d.strftime("%Y%m%d") #20210305
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+
+            data.save()
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context = { 
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
+            }
+            return redirect('checkout')
+    else:   
+        return redirect('checkout')
+
